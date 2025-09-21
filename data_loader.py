ea0b14d52f751ac46065f4a461fd93c41c0cade7
Irina Kuchieva <irina@MacBook-Pro-Irina.local>
2025-09-22 01:33:58 +0300

    HW2: add data_loader.py, deps, README; ignore datasets

--- data_loader.py
+++ data_loader.py
@@ -1,13 +1,10 @@
-#!/usr/bin/env python3
-
 import pandas as pd
 
 DATA_URL = "https://huggingface.co/datasets/aferikoglou/GNWSIS/resolve/main/GN%CE%A9SIS.csv"
 
 def main():
-    df = pd.read_csv(DATA_URL, encoding="utf-8", sep=",")
-    print(df.head(10).to_string(index=False))
+    raw_data = pd.read_csv(DATA_URL, encoding="utf-8", sep=",")
+    print(raw_data.head(10).to_string(index=False))  
 
 if __name__ == "__main__":
     main()
-
